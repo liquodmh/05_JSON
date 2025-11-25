098749abfcddb806ebd0c10dbb1fd9e8671412c8
mohamd alaa alden <66388156+liquodmh@users.noreply.github.com>
2025-11-25 18:06:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const list = document.getElementById('songList');
 const submitBtn = document.getElementById('submitBtn');
 
 //if it doesnt exist in localstorage get empty array else
-//get json text and convert it to object jason
+    //get json text and convert it to object jason
 
 let songs = JSON.parse(localStorage.getItem('playlist')) || [];
 
@@ -13,11 +13,11 @@ form.addEventListener('submit', (e) => {
     //Dont submit to server , i will handle it here
     e.preventDefault();
 
-    //read forms Data
+  //read forms Data
     const title = document.getElementById('title').value;
     const url = document.getElementById('url').value;
 
-    //create json bobj based on url title  
+//create json bobj based on url title  
     const song = {
         id: Date.now(),  // Unique ID
         title: title,
@@ -25,10 +25,38 @@ form.addEventListener('submit', (e) => {
         dateAdded: Date.now()
     };
 
-
+   
     songs.push(song);
 
     //TO DO SAVE  AND RERENDER 
 
     form.reset();
 });
+function saveAndRender(){
+    localStorage.setItem('playlist',JSON.stringify(songs));
+    //todo Reload UI
+    renderSongs(songs);
+}
+
+function renderSongs() {
+    list.innerHTML = ''; // Clear current list
+
+    songArray.forEach(song => {
+        // Create table row
+        const row = document.createElement('tr');
+        
+        row.innerHTML = `
+            <td>${song.title}</td>
+            <td><a href="${song.url}" target="_blank" class="text-info">Watch</a></td>
+            <td class="text-end">
+                <button class="btn btn-sm btn-warning me-2" onclick="editSong(${song.id})">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button class="btn btn-sm btn-danger" onclick="deleteSong(${song.id})">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </td>
+        `;
+        list.appendChild(row);
+    });
+}
